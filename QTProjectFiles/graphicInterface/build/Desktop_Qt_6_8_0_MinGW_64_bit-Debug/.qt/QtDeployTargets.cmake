bb3eb937d499175c39ed65dc6ef629a1c302a4ff
Lucas Komljenovic <21lerk@queensu.ca>
2024-11-21 17:32:02 -0500

    Add new folders for QT working files

---
+++ QTProjectFiles/graphicInterface/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_graphicInterface_FILE C:/Users/hawke/OneDrive/Desktop/CMPE320_Folder/Project/snackmap/QTProjectFiles/graphicInterface/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/graphicInterface.exe)
+set(__QT_DEPLOY_TARGET_graphicInterface_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_graphicInterface_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
